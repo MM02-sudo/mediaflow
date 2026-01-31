7f2beeea8e5e0e7c7acfe2c047b0d322b1cab05f
MM02-sudo <dandjimalcolm2@gmail.com>
2026-01-31 22:06:58 +0100

    start of server code

---
+++ server/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+	"encoding/json"
+	"os"
+	"path/filepath"
+	"github.com/MM02-sudo/mediaflow/shared"
+	"fmt"
+	""
+)
+
+//http server here
+func main(){
+
+
+}
+
+//request handler here
+
+func requestHandler(w http.ResponseWriter, request *http.Request)
+{
+
+}
