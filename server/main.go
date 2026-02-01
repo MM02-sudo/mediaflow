9d895c673a0c605e05a11c1f03fef26e1b6bfc93
MM02-sudo <dandjimalcolm2@gmail.com>
2026-02-01 21:35:56 +0100

    beginning of servers code

--- server/main.go
+++ server/main.go
@@ -7,12 +7,19 @@ import (
 	"path/filepath"
 	"github.com/MM02-sudo/mediaflow/shared"
 	"fmt"
-	""
+	"log"
+
 )
 
-//http server here
-func main(){
 
+func main(){
+	// this part tells the server when someone visits /
+	http.HandlerFunc("/", requestHandler)
+
+	// servers listens for requests on port 8080 it uses default settings(nil) and if it crashes it shows an error and exits with log.Fatal
+	
+	fmt.Println("Server starting on port: 8080...")
+	log.Fatal(http.ListenAndServe("8080", nil))
 
 }
 
