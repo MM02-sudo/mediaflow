3553a0b28f5e81a97c55db6b059a494da1b7b929
MM02-sudo <dandjimalcolm2@gmail.com>
2026-02-04 20:16:55 +0100

    small update

--- client/main.go
+++ client/main.go
@@ -12,13 +12,12 @@ import (
 
 func main() {
 	server := flag.String("server", "", "Server address(e.g., 192.168.1.100:8080)")
-	action := flag.String("action", "list", "Action: list or Stream")
+	action := flag.String("action", "list", "Action: list or stream")
 	path := flag.String("path", "/", "File or Folder path")
 
 	flag.Parse()
 
 	// lets check if server was provided
-
 	if *server == "" {
 		fmt.Println("Error: -server is required")
 		flag.Usage()
@@ -31,25 +30,23 @@ func main() {
 		Path:   *path,
 	}
 
-	// converting request in to json
+	// converting request into json
 	jsonData, err := json.Marshal(req)
 	if err != nil {
-		fmt.Println("error while creating request", err)
+		fmt.Println("Error while creating request:", err)
 		return
 	}
 
 	// sending an http POST to server
-
 	url := fmt.Sprintf("http://%s/", *server)
 	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
 	if err != nil {
 		fmt.Println("Error connecting to server:", err)
 		return
 	}
-
 	defer resp.Body.Close()
 
-	// 4.Read the response
+	// Read the response
 	var response shared.Response
 	err = json.NewDecoder(resp.Body).Decode(&response)
 	if err != nil {
@@ -62,22 +59,25 @@ func main() {
 
 func displayResponse(resp shared.Response) {
 	if !resp.Success {
-		fmt.Println("		Error:", resp.Error)
+		fmt.Println("Error:", resp.Error)
 		return
 	}
-	fmt.Println("		Success!")
+
+	fmt.Println("Success!")
 
 	if len(resp.Files) > 0 {
-		fmt.Println("\n		Files:")
+		fmt.Println("\nFiles and Folders:")
 		for _, file := range resp.Files {
 			if file.IsDir {
-				fmt.Printf("  %s\n", file.Name)
+				fmt.Printf("  [DIR]  %s\n", file.Name)
 			} else {
-				fmt.Printf("	%s\n", file.Name)
+				fmt.Printf("  [FILE] %s\n", file.Name)
 			}
 		}
 	}
+
 	if resp.StreamURL != "" {
-		fmt.Println("\n Stream URL:", resp.StreamURL)
+		fmt.Println("\nStream URL:", resp.StreamURL)
+		fmt.Println("Open this URL in VLC or your browser to play the video!")
 	}
 }
